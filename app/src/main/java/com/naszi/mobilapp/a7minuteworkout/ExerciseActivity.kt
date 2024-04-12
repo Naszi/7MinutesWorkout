2cdbb91f1d1dcd2aee1b64e1f6d90b2235c1dbb4
naszi <naszi75@gmail.com>
2024-04-12 14:21:35 +0200

    Added view binding to exercise screen and set up toolbar

--- app/src/main/java/com/naszi/mobilapp/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/naszi/mobilapp/a7minuteworkout/ExerciseActivity.kt
@@ -1,36 +1,31 @@
 package com.naszi.mobilapp.a7minuteworkout
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.FrameLayout
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.naszi.mobilapp.a7minuteworkout.databinding.ActivityExerciseBinding
 import com.naszi.mobilapp.a7minuteworkout.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private var binding: ActivityMainBinding? = null
+class ExerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExerciseBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding?.flStart?.setOnClickListener {
-            val intent = Intent(this, ExerciseActivity::class.java)
-            startActivity(intent)
+        setSupportActionBar(binding?.toolbarExercise)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        binding = null
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
