7c741970380884ae34d676ca76268e3de358ddcb
naszi <naszi75@gmail.com>
2024-04-12 08:21:51 +0200

    Added main screen UI

--- app/src/main/java/com/naszi/mobilapp/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/naszi/mobilapp/a7minuteworkout/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.naszi.mobilapp.a7minuteworkout
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Clicked the Button!",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
