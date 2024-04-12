8474f52a539554a6fb0fe4aa5c5a5ce85813f882
naszi <naszi75@gmail.com>
2024-04-12 13:40:50 +0200

    Added view binding to main screen

--- app/src/main/java/com/naszi/mobilapp/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/naszi/mobilapp/a7minuteworkout/MainActivity.kt
@@ -7,24 +7,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.naszi.mobilapp.a7minuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this,
                 "Clicked the Button!",
                 Toast.LENGTH_LONG
             ).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
