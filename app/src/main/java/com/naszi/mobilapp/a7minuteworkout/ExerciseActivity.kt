94ef3f380e4530a6cd09bacfe1af09c870cfdf5c
naszi <naszi75@gmail.com>
2024-04-13 07:39:24 +0200

    Added counter and setup progress bar

--- app/src/main/java/com/naszi/mobilapp/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/naszi/mobilapp/a7minuteworkout/ExerciseActivity.kt
@@ -1,6 +1,9 @@
 package com.naszi.mobilapp.a7minuteworkout
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,9 @@ import com.naszi.mobilapp.a7minuteworkout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,5 +33,43 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+        setupRestView()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
+    }
+
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }.start()
+    }
+
 }
