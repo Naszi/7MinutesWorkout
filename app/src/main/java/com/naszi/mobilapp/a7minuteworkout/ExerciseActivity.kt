58d068cde8fac9c95946ec4857f15651f2c69605
naszi <naszi75@gmail.com>
2024-04-17 14:23:52 +0200

    Added the upcoming exercise to rest screen and text to speech

--- app/src/main/java/com/naszi/mobilapp/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/naszi/mobilapp/a7minuteworkout/ExerciseActivity.kt
@@ -1,8 +1,9 @@
 package com.naszi.mobilapp.a7minuteworkout
 
-import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -10,9 +11,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.naszi.mobilapp.a7minuteworkout.databinding.ActivityExerciseBinding
-import com.naszi.mobilapp.a7minuteworkout.databinding.ActivityMainBinding
+import java.util.Locale
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var binding: ActivityExerciseBinding? = null
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
@@ -20,6 +21,7 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseProgress = 0
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
+    private var textToSpeech: TextToSpeech? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +38,22 @@ class ExerciseActivity : AppCompatActivity() {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
         exerciseList = Constants.defaultExerciseList()
+        textToSpeech = TextToSpeech(this, this)
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
         setupRestView()
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech!!.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The language specified is not supported")
+            }
+        } else {
+            Log.e("TTS", "Initialization Failed!")
+        }
     }
 
     override fun onDestroy() {
@@ -52,7 +66,15 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+        if (textToSpeech != null) {
+            textToSpeech?.stop()
+            textToSpeech?.shutdown()
+        }
         binding = null
+    }
+
+    private fun speakOut(text: String) {
+        textToSpeech?.speak(text, TextToSpeech.QUEUE_ADD, null, "")
     }
 
     private fun setupRestView() {
@@ -61,10 +83,13 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.INVISIBLE
         binding?.flExerciseView?.visibility = View.INVISIBLE
         binding?.ivImage?.visibility = View.INVISIBLE
+        binding?.tvUpComingLabel?.visibility = View.VISIBLE
+        binding?.tvUpComingExerciseName?.visibility = View.VISIBLE
         if (restTimer != null) {
             restTimer?.cancel()
             restProgress = 0
         }
+        binding?.tvUpComingExerciseName?.text = exerciseList!![currentExercisePosition +1].name
         setRestProgressBar()
     }
 
@@ -74,12 +99,15 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.VISIBLE
         binding?.flExerciseView?.visibility = View.VISIBLE
         binding?.ivImage?.visibility = View.VISIBLE
+        binding?.tvUpComingLabel?.visibility = View.INVISIBLE
+        binding?.tvUpComingExerciseName?.visibility = View.INVISIBLE
         if (exerciseTimer != null) {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
         binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].image)
         binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].name
+        speakOut(exerciseList!![currentExercisePosition].name)
         setExerciseProgressBar()
     }
 
